560c4b6457912c8f4cd552abe7b03bca88b5840e
Sean Boyle <sean94@gmail.com>
2019-10-08 15:52:31 +0100

    Setup Proxy to backend and Setup basic mongo connection to localhost

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
-  componentDidMount(){
-    axios.get('http://localhost:5000/api/users/test')
-      .then(result=>{
-        console.log(result.data.msg);
-        
-      })
+  componentDidMount() {
+    axios.get("/api/users/test").then(result => {
+      console.log(result.data.msg);
+    });
   }
   render() {
     return (
       <div className="container">
         <h1>React App</h1>
       </div>
-    )
+    );
   }
 }
-export default App
+export default App;
--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
+const db = process.env.MONGOURI || "mongodb://localhost:27017/socialweb";
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+const users = require("./api/v1/routes/users");
 
-const users = require('./api/v1/routes/users');
-
-
+//parse for the jsnon data that will be passed to the frontend clients
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-app.use('/api/users', users);
 
-
+app.use("/api/users", users);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

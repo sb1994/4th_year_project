fe3f18107e7c278797826e04354aa59d1921e20c
sb1994 <sean94@gmail.com>
2020-04-19 02:01:10 +0100

    Committed Redux Production Settings

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,17 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers";
-import thunkMiddleware from "redux-thunk";
+import { compose, createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+
 // import { persistStore, persistReducer } from "redux-persist";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // const persistConfig = {
 //   key: "root"
 // };
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunkMiddleware))
-);
-export default store;
+  composeWithDevTools(applyMiddleware(thunkMiddleware))
+)
+export default store
